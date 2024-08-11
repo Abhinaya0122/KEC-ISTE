2b0bb471f377ca32bb90b91cbfac6dec7fce3599
Abhinaya0122 <abhinayashrinivasan2004@gmail.com>
2024-08-11 23:10:20 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, redirect, jsonify, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
+
+@app.route('/event')
+def event():
+    return render_template('timeline.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
